ce90eec6f9c5ea23b365a1feee245b5a172bc0c8
David Chanin <chanindav@gmail.com>
2021-06-01 23:41:06 +0100

    slight tweaks

--- website/src/pages/IntroPage.tsx
+++ website/src/pages/IntroPage.tsx
@@ -14,32 +14,38 @@ const IntroPage = () => {
         dimension below to find out!
       </p>
       <div className="flex flex-wrap justify-center pt-10">
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink
             leftSense="Communism|NOUN"
             rightSense="Capitalism|NOUN"
           />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="east|NOUN" rightSense="west|NOUN" />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="north|NOUN" rightSense="south|NOUN" />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="USA|GPE" rightSense="France|GPE" />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="USA|GPE" rightSense="China|GPE" />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="smart|ADJ" rightSense="stupid|ADJ" />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="Tokyo|GPE" rightSense="New_York|GPE" />
         </div>
-        <div className="p-3">
+        <div className="p-2">
           <SenseDiffLink leftSense="happy|ADJ" rightSense="unhappy|ADJ" />
+        </div>
+        <div className="p-2">
+          <SenseDiffLink
+            leftSense="North_Korea|GPE"
+            rightSense="South_Korea|GPE"
+          />
         </div>
       </div>
     </div>
