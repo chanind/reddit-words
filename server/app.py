6480a6628da9badeb95afae2d65ee9be63461803
David Chanin <chanindav@gmail.com>
2021-06-01 17:45:36 +0100

    enabling CORS

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from api.routes import api
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(api, url_prefix="/")
     return app
 
